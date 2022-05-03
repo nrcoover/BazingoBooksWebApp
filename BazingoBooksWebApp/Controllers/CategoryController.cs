e4003c718797f000a8946a7d2a131e2b5d221cad
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-05-03 11:16:26 -0400

    Update CategoryController.cs
    Passes the objCategoryList to the View(), and also reassigns the objecCategoryList var as an IEnumerable, making the ToList() method no longer necessary.

--- BazingoBooksWebApp/Controllers/CategoryController.cs
+++ BazingoBooksWebApp/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BazingoBooksWebApp.Data;
+using BazingoBooksWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BazingoBooksWebApp.Controllers
@@ -14,8 +15,8 @@ namespace BazingoBooksWebApp.Controllers
 
         public IActionResult Index()
         {
-            var objCategoryList = _db.Categories.ToList();
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
         }
     }
 }
