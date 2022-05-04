a5576d9fcf4bbed13eb52d4c349871795209a5d6
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-05-03 21:59:01 -0400

    Adds Validation Error Messages to Front-End
    Adds validation code to ensure the user can't submit a Name and DisplayOrder with the same value.

--- BazingoBooksWebApp/Controllers/CategoryController.cs
+++ BazingoBooksWebApp/Controllers/CategoryController.cs
@@ -30,6 +30,11 @@ namespace BazingoBooksWebApp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
