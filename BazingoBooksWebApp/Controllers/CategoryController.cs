5abe1c110fd82b9c4dac33be64bf877e94fcb2b7
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-05-03 23:55:52 -0400

    Edits the Category Edit GET Action Method
    Adds the Edit GET and POST Action Methods, creates a VIEW for the Edit Action Methods, and adds connectivity and functionality between them.

--- BazingoBooksWebApp/Controllers/CategoryController.cs
+++ BazingoBooksWebApp/Controllers/CategoryController.cs
@@ -19,16 +19,54 @@ namespace BazingoBooksWebApp.Controllers
             return View(objCategoryList);
         }
 
-        // GET Action Method
+        // Create GET Action Method
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST Action Method
+        // Create POST Action Method
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Edit GET Action Method
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // Edit POST Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
