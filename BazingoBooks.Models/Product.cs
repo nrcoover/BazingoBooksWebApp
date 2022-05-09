8fcef5b38c991e99a879bde2c096cc1f6b859e34
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-05-09 18:55:18 -0400

    Create Product.cs
    Adds a new Class, "Product" to our models. Note: the Foreign Key attribute is included for reference, but commented out as it is not necessary thanks to the automated Foreign Key generated by the intelligence of the Entity Framework.

---
+++ BazingoBooks.Models/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazingoBooks.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+
+        public string ImageURL { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        //[ForeignKey("CategoryId")]
+        // Not necessary as the foreign key will be created automatically by the Entity Framework 
+        public Category Category { get; set; }
+
+        [Required]
+        public int CoverTypeId { get; set; }
+
+        //[ForeignKey("CoverTypeId")]
+        public CoverType CoverType { get; set; }
+    }
+}
